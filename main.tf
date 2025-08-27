3b73227ede8ce4555124111494b017dce10a6a3b
Vladimir Zdravkovic <v.zdravkovic@fitpassgroup.com>
2025-08-28 00:07:19 +0200

    Add deletion protection setting to GCP container cluster configuration

--- main.tf
+++ main.tf
@@ -63,6 +63,8 @@ resource "google_container_cluster" "primary" {
   location = var.region
   project  = var.project_id
 
+  deletion_protection = false
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
